1d96996a9b0bfdd2c0b1fbdbbeb31a5c7db9f002
Kevin Tran <ketr2@pdx.edu>
2023-11-14 17:15:27 -0800

    Provider.py Commit

---
+++ models/provider.py
@@ -0,0 +1,32 @@
+# Provider Model
+
+# Authorization Function
+"""
+This will be a boolean function. Checks the ID of the provider. If the provider ID is correct. 
+Returns a true statement. 
+def give_Auth():
+    if authorize function is True
+        don't say anything bc the provider was able to log in
+    else
+        print('You have entered a invalid ID. Try again.')
+        return false -> this could then call the authorize function again to ask the
+        provider to log in. 
+"""
+
+# Member Valid. Member Invalid.
+"""
+For the valid and invalid functions, it would call the check member_ID function to see if it
+returned a true statement or not and send a message accordingly.
+
+def message_ID():
+    if check_ID is True
+        print("Validated")
+    else
+        print("Invalidated")
+"""
+
+# Provider Directory
+"""
+Check the function that asks for accessing the directory. If that function returns a true
+statement. Then this function will display a JSON file. Maybe?
+"""
