affb705ca5058701de2fff445f13bfd26533178c
Kevin Tran <ketr2@pdx.edu>
2023-11-14 17:39:05 -0800

    Provider.py Commit v.2

--- models/provider.py
+++ models/provider.py
@@ -4,13 +4,14 @@
 """
 This will be a boolean function. Checks the ID of the provider. If the provider ID is correct. 
 Returns a true statement. 
-def give_Auth():
-    if authorize function is True
+def give_Auth(int provider_ID):
+    if checkProvderID function is True
         don't say anything bc the provider was able to log in
     else
         print('You have entered a invalid ID. Try again.')
-        return false -> this could then call the authorize function again to ask the
-        provider to log in. 
+        return false.
+        the authorize function from the view can use this as a way to tell the provider to 
+        log in again if they entered it in wrong.
 """
 
 # Member Valid. Member Invalid.
@@ -18,12 +19,28 @@ def give_Auth():
 For the valid and invalid functions, it would call the check member_ID function to see if it
 returned a true statement or not and send a message accordingly.
 
-def message_ID():
-    if check_ID is True
-        print("Validated")
+def message_ID(int memberID):
+    if checkMemberID is True
+        if memberRecievedService is True
+            If they did pay their service
+            print("Validated")
+            else
+                print("Suspended")
     else
         print("Invalidated")
 """
+
+# Message Fee
+"""
+Will use the getServiceFee function and return the fee from that function
+def message_Fee(int serviceCode)
+    if getServiceFee is True
+        return the fee
+
+    else
+        print("Enter the code again")
+"""
+
 
 # Provider Directory
 """
