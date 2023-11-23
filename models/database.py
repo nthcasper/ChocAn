e5aa2de04b4d363aea6b2c54054d8ac13a8e3fab
Simon Parker <siparker@pdx.edu>
2023-11-22 22:55:55 -0800

    first half of functions implemented

--- models/database.py
+++ models/database.py
@@ -2,12 +2,22 @@
 
 import json
 
+
+
+provDirPath = "../example_database/provider_directory.json"
+provRegPath = "../example_database/provider_registry.json"
+memRegPath = "../example_database/member_registry.json"
+servRecordsPath = "../example_database/service_records/"
+reportsPath = "../example_database/reports/"
+
+
 #this function will create a file consisting of a single JSON object at a given path.
 #If a file exists there, it will overwrite that file
 def createJSONFile(pathname, dictionary):
 	#open/create a file for writing at the specified path
 	#convert dict to JSON
 	#write JSON to file
+	pass
 
 
 #this function will create a file consisting of a list of JSON objects at a given path
@@ -17,46 +27,70 @@ def createJSONListFile(pathname, list_of_dictionaries):
 	#make new list
 	#for every dict in given list, add JSON to new list
 	#write new list to file
+	pass
 
 
 #this function will return a dictionary from a JSON file located at pathname. If the file is not a single
 #JSON object, or if the file does not exist, it will throw an error (maybe different errors for each)
 def getJSONDict(pathname):
-	#checks the validity of the path
-	#loads data from file
-	#converts to dict
-	#returns the dict
+	try:
+		with open(pathname, "r") as file:
+			dictionary = json.load(file)
+			if (type(dictionary) != dict):
+				raise ValueError
+
+	except IOError:
+		print("ERROR: file not found")
+		raise
+	except ValueError:
+		print("ERROR: file is not a valid .json dictionary")
+		raise
+
+	pass
 
 
 
 #this function will return a list of dictionaries from a JSON list file. If the file is not a list of JSON objects,
 #or the file does not exist, it will throw an error.
 def getJSONListOfDicts(pathname):
-	#check the validity of the pathname
-	#load data from the file
-	#make a new list, for each JSON in the old list, make a dict in the new list
-	#return the list
+	try:
+		with open(pathname, "r") as file:
+			listOfDicts = json.load(file)
+			if (type(listOfDicts) != list):
+				raise ValueError
+
+	except IOError:
+		print("ERROR: file not found")
+		raise
+	except ValueError:
+		print("ERROR: file is not a valid .json list")
+		raise
+
 
 
 
 #this function checks if a provider ID is valid, returning true or false depending 
 def checkProviderID(ID):
-	#load the provider registry list
-	#check each element for a match to the provided ID
-	#return true or false 
-	pass
-
+	with open(provRegPath, "r") as file:
+		providerList = json.load(file)
+		file.close()
+		for provider in providerList:
+			if int(provider['ID']) == int(ID): 
+				return True
+		return False
 
 
 
 
 #this function checks if a member ID is valid, returning the status of that member if they exist, or "Invalid" if the ID is invalid
 def checkMemberID(ID):
-	#load the member registry list
-	#check each element for a match
-	#if one is found, return the status field of that json object, ex. Valid or Suspended or whatever else
-	#if no match, return Invalid
-	pass
+	with open(memRegPath, "r") as file:
+		memberList = json.load(file)
+		file.close()
+		for member in memberList:
+			if int(member['ID']) == int(ID):
+				return member['status']
+		return "Invalid"		
 
 
 
@@ -64,12 +98,13 @@ def checkMemberID(ID):
 
 #this function checks if a given service code is valid, returning the true or false depending 
 def checkServiceCode(serviceCode):
-	#load the provider directory list
-	#for each element, check for a match against the given code
-	#return true or false to indicate any matches
-	pass
-
-
+	with open(provDirPath, "r") as file:
+		provDir = json.load(file)
+		file.close()
+		for service in provDir:
+			if int(service['code']) == int(serviceCode):
+				return True
+		return False
 
 
 
