e04f915564f5704800277902a7adaa21ab4c16aa
Kevin Tran <ketr2@pdx.edu>
2023-11-29 21:48:28 -0800

    Final Commit I swear

--- provider_controller.py
+++ provider_controller.py
@@ -1,22 +1,12 @@
 import datetime
-import database as db  # im not sure how we are going to change this yet
-# import __init__
-import os
-import sys
-script_dir = os.path.dirname(__file__)
-mymodule_dir = os.path.join(script_dir)
-sys.path.append(mymodule_dir)
-# from models import database as db
-# #im not sure how we are going to change this yet
+from database import *
 
 
 class ProviderControl:
     def __init__(self):
         # Initialize with file paths for the provider directory and service records
-        # might need to move this around depending on how were are keeping the files, like in folders or not
-        self.provider_directory = 'example_database\\provider_directory.json'
-        # self.service_records_file = 'example_database\\week_1-1_records.json'
-        # #might need to move this around depending on how were are keeping the files, like in folders or not
+        self.providerDirectory = 'database\provider_directory.json'
+        # Ensure this path is correct
 
     def giveAuthorization(self, providerId):
         """
@@ -24,23 +14,18 @@ class ProviderControl:
         Checks if the provider ID is exactly 9 digits.
         Returns True if valid, False otherwise.
         """
-        '''if len(providerId) != 9 or not providerId.isdigit():
-            return False'''
-        return db.checkProviderID(providerId)
+        if len(providerId) != 9 or not providerId.isdigit():
+            return False
+        return checkProviderID(providerId)
 
     def messageMemberId(self, memberId):
         """
-        Check the status of member ID and returns a status message.
+        Checks the status of member ID and returns a status message.
         Possible return statuses are "Valid", "Invalid", and "Suspended".
         """
-        '''if len(memberId) != 9 or not memberId.isdigit():
-            return "Invalid"  # Member ID must be a 9-digit number'''
-
-        memberStatus = db.checkMemberID(memberId)
-        if memberStatus:
-            return memberStatus  # actual status returned from the database
-        else:
-            return "Invalid"  # if the database check does not return a status, assume "Invalid"
+        if len(memberId) != 9 or not memberId.isdigit():
+            return "Invalid"  # Member ID must be a 9-digit number
+        return checkMemberID(memberId)
 
     def logout(self):
         """
@@ -54,20 +39,21 @@ class ProviderControl:
         Retrieves the provider directory from a JSON file.
         Returns a list of service details.
         """
-        return db.getJSONListOfDicts(self.provider_directory)
+        return getJSONListOfDicts(self.providerDirectory)
 
     def verifyServiceFee(self, serviceFee, serviceCode):
         """
         Verifies if the provided service fee matches the service code.
         Returns True if the fee is correct, False otherwise.
+        int(service['code']) == int(serviceCode)
         """
-        if not db.checkServiceCode(serviceCode):
+        if not checkServiceCode(serviceCode):
             return False
-
-        provider_directory = db.getJSONListOfDicts(self.provider_directory)
-        for service in provider_directory:
-            if service['code'] == serviceCode and str(service['fee']) == serviceFee:
+        providerDirectory = getJSONListOfDicts(self.providerDirectory)
+        for service in providerDirectory:
+            if int(service['code']) == serviceCode and int(service['fee']) == serviceFee:
                 return True
+        # print("Service Fee Not Found for Code:", serviceCode)  # Debug print I think this function is not working properly
         return False
 
     def verifyServiceCode(self, serviceCode):
@@ -77,7 +63,7 @@ class ProviderControl:
 
         """
         try:
-            providerDirectory = db.getJSONListOfDicts(self.providerDirectory)
+            providerDirectory = getJSONListOfDicts(self.providerDirectory)
             # Convert serviceCode to integer for comparison
             serviceCode = int(serviceCode)
             # Check if any service in the directory matches the provided service code
@@ -122,8 +108,8 @@ class ProviderControl:
         Retrieves the service fee for a given service code from the provider directory.
         Returns the service fee if found, None otherwise.
         """
-        provider_directory = self.getProviderDirectory()
-        for service in provider_directory:
+        providerDirectory = getJSONListOfDicts(self.providerDirectory)
+        for service in providerDirectory:
             if service['code'] == serviceCode:
                 return service['fee']
         return None
